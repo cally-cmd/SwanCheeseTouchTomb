96618cc54dc31b98bb5fedf8711b66e1d3e0fdf5
Justin Grubbs <grubbsjr@hendrix.edu>
2023-03-06 20:21:28 -0600

    Added GameManager Script

---
+++ SwanCheeseTouchTomb/Assets/Scripts/GameManager.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance {get; private set;}
+    public GameObject titleScreen;
+    public GameObject creditsScreen;
+    public GameObject tutorialScreen;
+    public GameObject endScreen;
+    public GameObject canvas;
+    public GameObject eventSystem;
+    public GameObject curtain;
+    private bool raiseLower = false;
+
+
+// Reused from previous lab
+
+    void Awake() {
+            if (Instance == null) {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+                DontDestroyOnLoad(canvas);
+                DontDestroyOnLoad(eventSystem);
+            } else {
+                Destroy(gameObject);
+                Destroy(canvas);
+                Destroy(eventSystem);
+            }
+
+        }
+
+    IEnumerator ColorLerpFunction(bool fadeout, float duration)
+    {
+        float time = 0;
+        raiseLower = true;
+        Image curtainImg = curtain.GetComponent<Image>();
+        Color startValue;
+        Color endValue;
+        if (fadeout) {
+            startValue = new Color(0, 0, 0, 0);
+            endValue = new Color(0, 0, 0, 1);
+        } else {
+            startValue = new Color(0, 0, 0, 1);
+            endValue = new Color(0, 0, 0, 0);
+        }
+
+        while (time < duration)
+        {
+            curtainImg.color = Color.Lerp(startValue, endValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        curtainImg.color = endValue;
+        raiseLower = false;
+    }
+
+     IEnumerator LoadYourAsyncScene(string scene)
+     {
+        StartCoroutine(ColorLerpFunction(true, 1));
+
+        while (raiseLower)
+        {
+            yield return null;
+        }
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        StartCoroutine(ColorLerpFunction(false, 1));
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
