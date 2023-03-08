4ea3351e07960acc41bf7d8a540a7a22ba747b51
Justin Grubbs <grubbsjr@hendrix.edu>
2023-03-07 18:06:08 -0600

    Added Dialog Trigger for Maze 1

---
+++ SwanCheeseTouchTomb/Assets/Scripts/DialogActivation.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogActivation : MonoBehaviour
+{
+
+    public int level;
+
+
+    public void OnTriggerEnter2D(Collider2D collider2D) {
+        print("Entered..");
+        if (collider2D.CompareTag("PlayerSwan")) {
+            if (level == 1) {
+                GameManager.Instance.DialogShow("Try to reach the end of the maze while avoiding mean ducks.");
+            } else if (level == 2) {
+                GameManager.Instance.DialogShow("There are more foul in this area. Be wary!");
+            } else {
+                GameManager.Instance.DialogShow("You're nearing the end, but this area is overrun with fowl.");
+            }
+        }
+    }
+    public void OnTriggerExit2D(Collider2D collider2D) {
+        if (collider2D.gameObject.CompareTag("PlayerSwan")) {
+            GameManager.Instance.DialogHide();
+        }
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
